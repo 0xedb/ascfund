0f996fc5563edb512b800ce311779f632f90afd2
theBashShell <edohbruno@gmail.com>
2019-10-26 23:12:49 +0000

    files ignored correctly

---
+++ README.md
@@ -0,0 +1,1 @@
+[Fund ASC](https://fundasc.ga)
---
+++ src/components/Loginform.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Loginform() {
+    return (
+        <div>
+            <form method="POST" action="">
+            
+            </form>
+        </div>
+    )
+}
+
+export default Loginform
